23a7bf4311de94db13baab60ebb9ce309aa0b0e9
stephane-klein <contact@stephane-klein.info>
2025-02-13 23:40:57 +0100

    WIP

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -35,11 +35,23 @@ docker compose up -d postgres2 --wait
 docker compose down postgres2
 
 sleep 2
-
 ./scripts/generate_dummy_rows_in_postgres1.sh
-
 docker compose exec backup-sidecar sh -c "pg_basebackup -U \${POSTGRES_USER} -h \${POSTGRES_HOST} -D /backup/\$(date \"+%Y%m%d_%H%M%S\")_incr/ -l backup -P -v --incremental=/backup/\$(ls -1r /backup/ | head -n1)/backup_manifest"
 
+sleep 2
+./scripts/generate_dummy_rows_in_postgres1.sh
+docker compose exec backup-sidecar sh -c "pg_basebackup -U \${POSTGRES_USER} -h \${POSTGRES_HOST} -D /backup/\$(date \"+%Y%m%d_%H%M%S\")_incr/ -l backup -P -v --incremental=/backup/\$(ls -1r /backup/ | head -n1)/backup_manifest"
+
+sleep 2
+./scripts/generate_dummy_rows_in_postgres1.sh
+docker compose exec backup-sidecar sh -c "pg_basebackup -U \${POSTGRES_USER} -h \${POSTGRES_HOST} -D /backup/\$(date \"+%Y%m%d_%H%M%S\")_incr/ -l backup -P -v --incremental=/backup/\$(ls -1r /backup/ | head -n1)/backup_manifest"
+
+sleep 2
+./scripts/generate_dummy_rows_in_postgres1.sh
+docker compose exec backup-sidecar sh -c "pg_basebackup -U \${POSTGRES_USER} -h \${POSTGRES_HOST} -D /backup/\$(date \"+%Y%m%d_%H%M%S\")_incr/ -l backup -P -v --incremental=/backup/\$(ls -1r /backup/ | head -n1)/backup_manifest"
+
+sleep 2
+docker compose exec backup-sidecar sh -c "pg_basebackup -U \${POSTGRES_USER} -h \${POSTGRES_HOST} -D /backup/\$(date \"+%Y%m%d_%H%M%S\")_incr/ -l backup -P -v --incremental=/backup/\$(ls -1r /backup/ | head -n1)/backup_manifest"
 
 docker compose exec backup-sidecar sh -c "rm -rf /var/lib/postgres2/data/*; rm -rf /var/lib/postgres2/data/.* 2>/dev/null ; pg_combinebackup \$(cd /backup && ls -1 | sort | sed 's#^#/backup/#' | tr '\n' ' ') -o /var/lib/postgres2/data/ ; chown -R 999:999 /var/lib/postgres2/data/ ; ls -lha /var/lib/postgres2/data/"
 
@@ -48,3 +60,4 @@ docker compose up -d postgres2 --wait
 ./scripts/postgres2-display-dummy-rows.sh
 
 docker compose exec backup-sidecar sh -c "du -h -d1 /backup/"
+docker compose exec backup-sidecar sh -c "du -h -d0 /var/lib/postgres2/data/"
--- scripts/seed.sh
+++ scripts/seed.sh
@@ -6,8 +6,9 @@ cd "$(dirname "$0")/../"
 docker compose exec -T postgres1 sh -c "cat << 'EOF' | psql -U postgres
 DROP TABLE IF EXISTS public.dummy CASCADE;
 CREATE TABLE public.dummy (
-    id                     SERIAL PRIMARY KEY,
-    text                   VARCHAR NULL
+    id       SERIAL PRIMARY KEY,
+    text     VARCHAR NULL,
+    _blob    TEXT
 );
 
 DROP FUNCTION IF EXISTS public.insert_dummy_records;
@@ -17,8 +18,12 @@ CREATE FUNCTION public.insert_dummy_records(
 LANGUAGE 'plpgsql'
 AS \$\$
 BEGIN
-    INSERT INTO public.dummy (text)
-    SELECT NOW() FROM GENERATE_SERIES(1, number_rows);
+    INSERT INTO public.dummy (text, _blob)
+    SELECT 
+        NOW(),
+        (SELECT string_agg(md5(i::text || random()::text), '')
+         FROM generate_series(1, 256) i)::TEXT -- ~10Ko de données aléatoires
+    FROM GENERATE_SERIES(1, number_rows);
 END;
 \$\$;
 \q
